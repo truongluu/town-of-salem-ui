6d8ec4ecab38bdf04a2539dfa7a8bc99b6f2bbce
vunguyenhung <vunguyenhung@outlook.com>
2018-01-01 14:17:19 +0700

    make button outlined

--- src/components/authentication.component.js
+++ src/components/authentication.component.js
@@ -52,7 +52,7 @@ const Authentication = props => (
 							</button>
 							<button
 								type="button"
-								className="btn btn-primary"
+								className="btn btn-outline-primary"
 								onClick={() =>
 									props.onLogin({ username: props.username, password: props.password })}
 							>
