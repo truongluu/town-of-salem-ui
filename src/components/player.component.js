611f6303405e47e1f2360d1245747ec673279a90
vunguyenhung <vunguyenhung@outlook.com>
2018-01-02 15:58:44 +0700

    fix styles

--- src/components/player.component.js
+++ src/components/player.component.js
@@ -4,23 +4,24 @@ import RoleInformation from './role-information.component';
 import InteractionResults from './interaction-results.component';
 
 const Player = props => (
-	<div className="card col-md-3">
-		<div className="card-body">
-			<div>
-				<span className="lead mr-2">Player: {props.player.username}</span>
-				{props.player.died && <span className="badge badge-danger">Died</span>}
+	<div className="col-md-3 mb-3">
+		<div>
+			<span className="lead mr-2">Player: <b>{props.player.username}</b></span>
+			{props.player.died && <span className="badge badge-danger">Died</span>}
+		</div>
+		<div className="card">
+			<div className="card-body">
+				<div className="lead">Role: <b>{props.player.role}</b></div>
+				<RoleInformation role={props.player.role} />
+				<hr />
+				{!props.reconnect && <LastWill
+					disabled={props.player.died}
+					currentPlayerLastWill={props.player.lastWill}
+					onUpdateLastWill={props.onUpdateLastWill}
+				/>}
+				<hr />
+				<InteractionResults interactionResults={props.player.interactionResults} />
 			</div>
-			<hr />
-			<div className="lead">Role: <b>{props.player.role}</b></div>
-			<RoleInformation role={props.player.role} />
-			<hr />
-			{!props.reconnect && <LastWill
-				disabled={props.player.died}
-				currentPlayerLastWill={props.player.lastWill}
-				onUpdateLastWill={props.onUpdateLastWill}
-			/>}
-			<hr />
-			<InteractionResults interactionResults={props.player.interactionResults} />
 		</div>
 	</div>
 );
